9c2bfcf8af1951c7978b6f86d6097f7083935765
Benjamin-Re <renker.benjamin@gmail.com>
2022-03-28 12:13:08 +0200

    Refactor using Classes

--- app.js
+++ app.js
@@ -1,116 +1,103 @@
-let myLibrary = [];
-let i = 0;
-
-// the constructor...
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-// Store a new book in myLibrary
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
-  createCard();
-}
-
-// Add a book manually to myLibrary
-const starterBook = {
-  title: "Fiesta",
-  author: "Hemmingway",
-  pages: 200,
-  read: "true",
-};
-myLibrary.push(starterBook);
-
-// Populate the card that already is in the html with the starter book
-populateCard(
-  0,
-  myLibrary[0]["title"],
-  myLibrary[0]["author"],
-  myLibrary[0]["pages"],
-  myLibrary[0]["read"]
-);
-// The starter book doesn't add a delete event listener bc it serves as clone
-
-// Populate a card with the book data
-function populateCard(n, t, a, p, r) {
-  let title = document.querySelector(`.num${n} .card-title`);
-  title.textContent = t;
-
-  let author = document.querySelector(`.num${n} .card-subtitle`);
-  author.textContent = a;
-
-  let pages = document.querySelector(`.num${n} p:first-of-type`);
-  pages.textContent = p;
-
-  let read = document.querySelector(`.num${n} input[type='checkbox']`).checked=r;
-  read.textContent = r;
-}
-
-// Enable user to add a new book from user input
-const form = document.getElementById("addBook");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  title = form.elements[0].value;
-  author = form.elements[1].value;
-  pages = form.elements[2].value;
-  read = form.elements[3].checked;
-  addBookToLibrary(author, title, pages, read);
-  // Clear the form fields (because we can't refresh the page without loosing our data)
-  clearInputs();
-});
-
-// Add a second book manually for test purposes
-addBookToLibrary("Fortune de France", "Robert Merle", 600, true);
-
-function clearInputs() {
-  const inputs = document.querySelectorAll("input");
-  for (const input of inputs) {
-    input.value = "";
+// Book class
+class Book {
+  title;
+  author;
+  pages;
+  read;
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
 }
 
-// Display the books in myLibrary as cards
-function createCard() {
-  i++;
-  // Create a card for each book
-  let clone = document.querySelector(".card").cloneNode(true);
-
-  clone.classList.remove(`num0`);
-  console.log(clone.classList)
-  clone.classList.add(`num${i}`);
-  document.querySelector(".card-container").appendChild(clone);
-
-  // Update the data of the clone
-  populateCard(
-    i,
-    myLibrary[i]["title"],
-    myLibrary[i]["author"],
-    myLibrary[i]["pages"],
-    myLibrary[i]["read"]
-  );
-  // Add an event listener to its delete link
-  allowDeleteCard();
+// Library class
+class Library {
+  myLibrary = [];
+  addBookToLibrary(book) {
+    this.myLibrary.push(book);
+  }
+  getBook(index) {
+    return this.myLibrary[index];
+  }
+  deleteBook(index){
+    this.myLibrary.pop(index);
+  }
 }
 
-// Add delete function to delete link
-function allowDeleteCard() {
-  // Get the delete link
-  const deleteLink = document.querySelector(`.num${i} .delete`);
+// View Controller
+class ViewController {
+  myLibrary = new Library();
 
-  
-  // Add an event listener
-  deleteLink.addEventListener("click", (event) => {
-    console.log("Clicked on" + event.currentTarget);
-    // Delete the book ( with class .num${i}) from myLibrary
-    let indexToDelete = event.currentTarget.parentNode.parentNode.classList[1].substring(3);
-    myLibrary.pop(indexToDelete);
-    // Delete the card from the .card-container
-    let cardContainer = event.currentTarget.parentNode.parentNode.parentNode;
-    cardContainer.removeChild(event.currentTarget.parentNode.parentNode);
-  });
-  
+  populateCard(n, book) {
+    let title = document.querySelector(`.num${n} .card-title`);
+    title.textContent = book.title;
+
+    let author = document.querySelector(`.num${n} .card-subtitle`);
+    author.textContent = book.author;
+
+    let pages = document.querySelector(`.num${n} p:first-of-type`);
+    pages.textContent = book.pages;
+
+    let read = (document.querySelector(
+      `.num${n} input[type='checkbox']`
+    ).checked = book.read);
+    // read.textContent = book.read;
+  }
+
+  initialize() {
+    // Add a starter book to the lib
+    const starterBook = new Book("Fiesta", "Hemmingway", 200, "true");
+    this.myLibrary.addBookToLibrary(starterBook);
+    this.populateCard(0, this.myLibrary.getBook(0));
+    // Enable user to add a new book using the form
+    const form = document.getElementById("addBook");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let title = form.elements[0].value;
+      let author = form.elements[1].value;
+      let pages = form.elements[2].value;
+      let read = form.elements[3].checked;
+      const tempBook = new Book(title, author, pages, read);
+      this.myLibrary.addBookToLibrary(tempBook);
+      this.createCard(tempBook);
+      // Clear the form fields (because we can't refresh the page without loosing our data)
+      this.clearInputs();
+    });
+  }
+
+  clearInputs() {
+    const inputs = document.querySelectorAll("input");
+    for (const input of inputs) {
+      input.value = "";
+    }
+  }
+
+  createCard(tempBook) {
+    let i;
+    // Create a card for each book
+    let clone = document.querySelector(".card").cloneNode(true);
+    clone.classList.remove(`num0`);
+    console.log(clone.classList);
+    clone.classList.add(`num${i++}`);
+    document.querySelector(".card-container").appendChild(clone);
+    this.populateCard(i, tempBook);
+    // Add event listener to the delete button of this new card
+    const deleteLink = document.querySelector(`.num${i} .delete`);
+    deleteLink.addEventListener("click", (event) => {
+      console.log("Clicked on" + event.currentTarget);
+      // Delete the book ( with class .num${i}) from myLibrary
+      let indexToDelete =
+        event.currentTarget.parentNode.parentNode.classList[1].substring(3);
+      this.myLibrary.deleteBook(indexToDelete);
+      // Delete the card from the .card-container
+      let cardContainer = event.currentTarget.parentNode.parentNode.parentNode;
+      cardContainer.removeChild(event.currentTarget.parentNode.parentNode);
+    });
+  }
 }
+
+const myViewController = new ViewController();
+myViewController.initialize();
 
